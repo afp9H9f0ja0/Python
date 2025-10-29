de47cfb9b5d720c5217d6abea50f81f89a459c4a
afp9H9f0ja0 <stanislav.demchenko@student.sumdu.edu.ua>
2025-10-29 18:20:47 +0200

    Delete 3lab directory

--- 3lab/Завдання 2.py
+++
@@ -1,4 +0,0 @@
-word = input("Введіть слово: ")
-
-unique_chars = set(word)  
-print("Кількість різних символів у слові:", len(unique_chars))
--- 3lab/Завдання 3.py
+++
@@ -1,8 +0,0 @@
-sentence = input("Введіть речення: ")
-
-words = sentence.split()   
-
-print("Слова, відмінні від 'привіт':")
-for word in words:
-    if word.lower() != "привіт":
-        print(word)
